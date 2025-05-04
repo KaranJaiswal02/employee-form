8b8fbaded330f7b9b95c66ac82187a6b0a7ba630
KaranJaiswal02 <karanjaiswal3105@gmail.com>
2025-05-04 13:43:44 +0530

    feat: Enhance print forms with data handling and responsive design for Gratuity, ID Card, Nomination, and Staff Joining forms

--- src/components/print/GratuityForm.tsx
+++ src/components/print/GratuityForm.tsx
@@ -1,13 +1,29 @@
 "use client";
 import GratuityForm1 from '@/components/gratuityForm/GratuityForm1'
 import GratuityForm2 from '@/components/gratuityForm/GratuityForm2'
+import { DefaultGrauFormData } from '@/hooks/defaultValue'
+import { IGratuityForm } from '@/models/forms/gratuity-form'
+import { useAtom } from 'jotai'
+import { useEffect } from 'react'
+import { grauFormData } from '@/hooks/Atoms'
 
-export default function GratuityForm() {
+export default function GratuityForm({ data = DefaultGrauFormData }: { data?: IGratuityForm }) {
+  const [, setFormData] = useAtom(grauFormData)
+
+  useEffect(() => {
+    setFormData(data);
+  }, [data, setFormData]);
 
   return (
-    <div className="force-light p-6 max-w-4xl mx-auto bg-white">
-      <GratuityForm1 />
-      <GratuityForm2 />
-    </div>
+    <>
+      <div className='print:hidden h-screen text-5xl flex justify-center items-end bg-white dark:bg-card text-black dark:text-white p-4 font-bold font-mono'>
+        Gratuity Form
+      </div>
+
+      <div className="hidden print:block force-light p-6 max-w-4xl mx-auto bg-white">
+        <GratuityForm1 />
+        <GratuityForm2 />
+      </div>
+    </>
   )
 }
--- src/components/print/NominationForm2.tsx
+++ src/components/print/NominationForm2.tsx
@@ -1,13 +1,27 @@
 "use client";
 import Nomination1 from '@/components/nominationrevisedform/Nomination1';
 import Nomination2 from '@/components/nominationrevisedform/Nomination2';
+import { DefaultNominationForm2Data } from '@/hooks/defaultValue';
+import { NominationForm2Model } from '@/models/forms/nomination-form2';
+import { useAtom } from 'jotai';
+import { useEffect } from 'react';
+import { nominationForm2Data } from '@/hooks/Atoms';
 
-export default function NominationForm2() {
+export default function NominationForm2({ data = DefaultNominationForm2Data }: { data?: NominationForm2Model }) {
+    const [, setFormData] = useAtom(nominationForm2Data);
 
+    useEffect(() => {
+        setFormData(data);
+    }, [data, setFormData]);
     return (
-        <div className="force-light p-6 max-w-5xl mx-auto bg-white">
-            <Nomination1 />
-            <Nomination2 />
-        </div>
+        <>
+            <div className='print:hidden h-screen text-5xl flex justify-center items-end bg-white dark:bg-card text-black dark:text-white p-4 font-bold font-mono'>
+                Nomination & Declaration Form2
+            </div>
+            <div className="hidden print:block force-light p-6 max-w-5xl mx-auto bg-white">
+                <Nomination1 />
+                <Nomination2 />
+            </div>
+        </>
     );
 }
