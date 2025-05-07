d19982deb81b43b331a3ca158a89f5da39a591e2
tulsyanshubham <tulsyanshubham3639@gmail.com>
2025-05-07 13:57:32 +0530

    Refactor forms to use new components and improve submission handling
    - Replaced NominationForm2 with MyPage component in nomination declaration form.
    - Updated staff family members form to use MyPage component.
    - Refactored staff joining form to utilize MyPage component.
    - Changed thank you page to use MyPage component for consistency.
    - Removed obsolete BankMandate, GratuityForm, IdcardForm, NominationForm1, NominationForm2, StaffFamily, and StaffJoining components.
    - Enhanced error handling and submission logic across forms.
    - Improved user feedback with toast notifications on form submission.

--- src/app/forms/bank-mandate/page.tsx
+++ src/app/forms/bank-mandate/page.tsx
@@ -1,5 +1,93 @@
-import { Suspense } from 'react';
-import BankMandate from '@/components/forms/BankMandate';
+"use client";
+import { Suspense, useEffect, useState } from 'react';
+import { useRouter, useSearchParams } from 'next/navigation';
+import { useAtom } from 'jotai';
+import { bankMandateFormData, empFormData, formStatusus } from '@/hooks/Atoms';
+import { Button } from '@/components/ui/button';
+import { toast } from "sonner"
+import BankMandateForm from '@/components/bankMandateForm/BankMandateForm';
+import IError from '@/types/error';
+
+function BankMandate() {
+    const router = useRouter();
+    const [formData, setFormData] = useAtom(bankMandateFormData);
+    const [formData1] = useAtom(empFormData);
+    const [, setFormStatus] = useAtom(formStatusus);
+    const [isSubmitting, setIsSubmitting] = useState(false);
+    const [errors, setErrors] = useState<string[]>([]);
+    const [id, setId] = useState<string | null>(null);
+    //16-17 lagana hai sbmein
+    useEffect(() => {
+        const searchParams = useSearchParams()
+        setId(searchParams.get('id'));
+        setFormData((prev) => ({
+            ...prev,
+            name: formData1.name || "",
+            address: formData1.perAddress || "",
+        }));
+    }, [setFormData, formData1]);
+
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        setIsSubmitting(true);
+        try {
+            console.log(formData);
+            const response = await fetch("/api/forms/bank-mandate", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    "authorization": `Bearer ${localStorage.getItem("token")}`,
+                    "userid": id as string,
+                },
+                body: JSON.stringify(formData),
+            });
+            const responseData = await response.json();
+            if (responseData.success) {
+                setFormStatus((prevStatus) => ({
+                    ...prevStatus,
+                    bank_mandate: {
+                        ...prevStatus.bank_mandate,
+                        status: "done",
+                    },
+                }));
+                toast.success(responseData.message);
+                const params = id ? `?id=${id}` : '';
+                router.push(`/forms/nomination-declaration-form1${params}`);
+            }
+            else {
+                toast.error(responseData.message);
+                setErrors(responseData.errors);
+            }
+        } catch (err: unknown) {
+            const error = err as IError;
+            toast.error("Error submitting form", {
+                description: error.message || "An error occurred",
+            });
+        } finally {
+            setIsSubmitting(false);
+        }
+    };
+
+    return (
+        <form onSubmit={handleSubmit} className="p-6 max-w-4xl mx-auto bg-white dark:bg-card border shadow-md rounded-lg">
+            <BankMandateForm />
+
+            <div className="text-right font-bold mt-6 mb-4">SIGNATURE</div>
+            {errors.length > 0 && (
+                <div className="text-red-600 text-sm px-2 text-left">
+                    {errors.map((err, index) => (
+                        <div key={index}>{err}</div>
+                    ))}
+                </div>
+            )}
+            <div className="flex justify-center mt-6">
+                <Button type="submit" disabled={isSubmitting} className="w-full cursor-pointer">
+                    {isSubmitting ? "Submitting..." : "Submit"}
+                </Button>
+            </div>
+        </form >
+    );
+}
 
 export default function Page() {
     return (
--- src/app/forms/staff-family-members/page.tsx
+++ src/app/forms/staff-family-members/page.tsx
@@ -1,10 +1,105 @@
-import StaffFamily from "@/components/forms/StaffFamily";
-import { Suspense } from "react";
+"use client";
+import { Button } from "@/components/ui/button";
+import React, { Suspense, useEffect, useState } from "react";
+import { useRouter, useSearchParams } from "next/navigation";
+import { empFormData, formStatusus, staffFamilyFormData } from "@/hooks/Atoms";
+import { useAtom } from "jotai";
+import { toast } from "sonner";
+import StaffFamilyMembers from "@/components/staffFamilyMembers/StaffFamilyMembers";
+import { calculateAge } from "@/lib/calculateAge";
+import IError from "@/types/error";
+
+// type Child = {
+//     name: string;
+//     gender: string;
+//     dob: string;
+// };
+
+function MyPage() {
+    const router = useRouter();
+    const [formData, setFormData] = useAtom(staffFamilyFormData);
+    const [form1data] = useAtom(empFormData);
+    const [, setFormStatus] = useAtom(formStatusus);
+    const [isSubmitting, setIsSubmitting] = useState(false);
+    const [errors, setErrors] = useState<string[]>([]);
+    const [id, setId] = useState<string | null>(null);
+    
+    useEffect(() => {
+        const searchParams = useSearchParams()
+        setId(searchParams.get('id'));
+        setFormData((prev) => ({
+            ...prev,
+            name: form1data.name || "",
+            dob: form1data.dob || "",
+            department: form1data.department || "",
+            age: calculateAge(form1data.dob?.toString().split('T')[0] ?? '') || 0,
+        }));
+    }, []);
+
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        setIsSubmitting(true);
+        try {
+            console.log(formData);
+            const response = await fetch("/api/forms/staff-family-members", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    "authorization": `Bearer ${localStorage.getItem("token")}`,
+                    "userid": id as string,
+                },
+                body: JSON.stringify(formData),
+            });
+            const responseData = await response.json();
+            if (responseData.success) {
+                setFormStatus((prevStatus) => ({
+                    ...prevStatus,
+                    staff_family_members: {
+                        ...prevStatus.staff_family_members,
+                        status: "done",
+                    },
+                }));
+                toast.success(responseData.message);
+                const params = id ? `?id=${id}` : '';
+                router.push(`/forms/bank-mandate${params}`);
+            }
+            else {
+                toast.error(responseData.message);
+                setErrors(responseData.errors);
+            }
+        } catch (err: unknown) {
+            const error = err as IError;
+            toast.error("Error submitting form", {
+                description: error.message || "An error occurred",
+            });
+        } finally {
+            setIsSubmitting(false);
+        }
+    };
+
+    return (
+        <form onSubmit={handleSubmit} className="p-6 max-w-5xl mx-auto bg-white dark:bg-card border shadow-md rounded-lg">
+            <StaffFamilyMembers />
+            {errors.length > 0 && (
+                <div className="text-red-600 text-sm px-2 text-left">
+                    {errors.map((err, index) => (
+                        <div key={index}>{err}</div>
+                    ))}
+                </div>
+            )}
+            <div className="flex justify-center mt-2">
+                <Button type='submit' disabled={isSubmitting} className="mt-6 w-full cursor-pointer">
+                    {isSubmitting ? "Submitting..." : "Submit"}
+                </Button>
+            </div>
+        </form>
+    );
+}
 
 export default function Page() {
     return (
         <Suspense fallback={<p>Loading...</p>}>
-            <StaffFamily />
+            <MyPage />
         </Suspense>
     );
 }
