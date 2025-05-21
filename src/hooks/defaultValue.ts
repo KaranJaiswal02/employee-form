74e60a12591028dc7438bb25c447caa181d5f587
shtulsyan_deloitte <shtulsyan@deloitte.com>
2025-05-21 16:28:22 +0530

    feat: Update DefaultCurrentUserData to align with ICurrentUser interface role restrictions

--- src/hooks/defaultValue.ts
+++ src/hooks/defaultValue.ts
@@ -14,7 +14,7 @@ export const DefaultCurrentUserData : ICurrentUser = {
     id: "",
     name: "",
     email: "",
-    role: "",
+    role: null,
 }
 
 export const DefaultEmpFormData: IEmpFormData = {
