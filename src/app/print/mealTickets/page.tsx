b6710d44e815ccd105261786ebdd023aee1cfc1b
tulsyanshubham <tulsyanshubham3639@gmail.com>
2025-05-31 11:57:04 +0530

    feat: Extend print timeout and enhance meal ticket display for better user experience

--- src/app/print/mealTickets/page.tsx
+++ src/app/print/mealTickets/page.tsx
@@ -32,7 +32,7 @@ export default function MealTicketPrintPage() {
                 window.print();
                 localStorage.removeItem(LOCAL_STORAGE_KEY);
                 window.close();
-            }, 500);
+            }, 1000);
 
             return () => clearTimeout(printTimeout);
         } catch (err) {
@@ -44,35 +44,37 @@ export default function MealTicketPrintPage() {
     if (!data) return <div>Loading...</div>;
 
     const { names, month, year, fromDay, toDay } = data;
-    console.log(names, month, year, fromDay, toDay);
 
     return (
         <div className="w-full dark">
-            <div className="print:hidden h-screen text-xl md:text-3xl xl:text-5xl flex justify-center items-end bg-white dark:bg-card text-black dark:text-white p-4 font-bold font-mono">
-                Meal Tickets for {names} - {month} {year}
+            <div className="print:hidden h-screen text-xl md:text-3xl xl:text-5xl flex justify-center items-end bg-card text-white p-4 font-bold font-mono">
+                Meal Tickets for {names.length < 2 ? names[0] : `${names.length} People`} - {month} {year}
             </div>
             {names.map((name, index) => (
-                <div key={index} className="hidden print:grid grid-cols-4 gap-3 p-4 mx-auto mb-5">
-                    {Array.from({ length: toDay - fromDay + 1 }, (_, i) => {
-                        const ticketNo = fromDay + i;
-                        return (
-                            <div
-                                key={ticketNo}
-                                className="w-40 h-27 border border-gray-300 rounded-md bg-white shadow-sm p-2 flex flex-col justify-between relative break-inside-avoid"
-                            >
-                                <div className="absolute top-1 right-2 text-sm font-semibold">#{ticketNo}</div>
-                                <div className="flex justify-center h-12">
-                                    <img
-                                        src="/assets/images/SL India Software Center copy.png"
-                                        alt="Logo"
-                                        className="w-40 object-contain"
-                                    />
+                <div key={index}>
+                    <div className="hidden print:grid grid-cols-4 gap-3 p-4 mx-auto">
+                        {Array.from({ length: toDay - fromDay + 1 }, (_, i) => {
+                            const ticketNo = fromDay + i;
+                            return (
+                                <div
+                                    key={ticketNo}
+                                    className="w-40 h-27 border border-gray-300 rounded-md bg-white shadow-sm p-2 flex flex-col justify-between relative break-inside-avoid"
+                                >
+                                    <div className="absolute top-1 right-2 text-sm font-semibold">#{ticketNo}</div>
+                                    <div className="flex justify-center h-12">
+                                        <img
+                                            src="/assets/images/SL India Software Center copy.png"
+                                            alt="Logo"
+                                            className="w-40 object-contain"
+                                        />
+                                    </div>
+                                    <div className="text-center text-xs font-semibold text-gray-800 my-1">{name}</div>
+                                    <div className="text-center text-xs text-gray-600">{month} {year}</div>
                                 </div>
-                                <div className="text-center text-xs font-semibold text-gray-800 my-1">{name}</div>
-                                <div className="text-center text-xs text-gray-600">{month} {year}</div>
-                            </div>
-                        )
-                    })}
+                            )
+                        })}
+                    </div>
+                    <div className="page-break"></div>
                 </div>
             ))}
         </div>
