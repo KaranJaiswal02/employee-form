054fb5a383fd286d49dae19f18a7d482e3246671
Shubham Tulsyan <tulsyanshubham3639@gmail.com>
2025-06-04 18:36:21 +0530

    Update route.ts
    added error handling

--- src/app/api/(auth)/create-admin/route.ts
+++ src/app/api/(auth)/create-admin/route.ts
@@ -2,6 +2,10 @@ import { NextResponse } from "next/server";
 import { seedAdmin } from "@/lib/seedAdmin";
 
 export async function GET() {
-    const message = await seedAdmin() as string;
-    return NextResponse.json({ success: true, message });
+    try {
+        const message = await seedAdmin() as string;
+        return NextResponse.json({ success: true, message });
+    } catch (error) {
+        return NextResponse.json({ success: false, message: (error as Error).message || "An error occurred" }, { status: 500 });
+    }
 }
